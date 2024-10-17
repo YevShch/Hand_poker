7a4d67b88a3b1cf1287415dc6cb461db39e34947
YevShch <yevheniiashcherbakova82@gmail.com>
2024-10-17 14:23:29 +0200

    Update createHandWithSuits.js

--- hivat-tests/helpers/createHandWithSuits.js
+++ hivat-tests/helpers/createHandWithSuits.js
@@ -1,34 +1,22 @@
 import Card from "../../classes/Card";
 
 export default function createHandWithSuits ( { ranks, combinationType } ) {
-  let suits = [];
+  // Default suit assignments
+  const defaultSuits = [ '♠', '♥', '♦', '♣', '♠' ];
 
   // Assign suits based on the combination type
-  switch (combinationType) {
-    case 'SF':  // Straight Flush - all suits are the same
-    case 'F':   // Flush - all suits are the same
-      suits = Array(5).fill('♠'); // For example, all suits will be ♠ (or any same suit)
-      break;
-    case 'S':   // Straight - different suits
-      suits = ['♠', '♥', '♦', '♣', '♠']; // Different suits
-      break;
-    case '4K':  // Four of a Kind - suits can be different
-    case 'FH':  // Full House - suits can be different
-    case '3K':  // Three of a Kind - suits can be different
-    case '2P':  // Two Pair - suits can be different
-    case '1P':  // One Pair - suits can be different
-    case 'HC':  // High Card - suits can be different
-      suits = ['♠', '♥', '♦', '♣', '♠']; // Different suits
-      break;
-    default:
-      suits = ['♠', '♥', '♦', '♣', '♠']; // Default to different suits
+  let suits;
+
+  if ( [ 'SF', 'F' ].includes( combinationType ) ) {
+    suits = Array( 5 ).fill( '♠' );  // Straight Flush or Flush - all suits are the same
+  } else if ( [ 'S', '4K', 'FH', '3K', '2P', '1P', 'HC' ].includes( combinationType ) ) {
+    suits = defaultSuits;  // All these combinations allow different suits
+  } else {
+    throw new Error( `Invalid combination type: ${ combinationType }` );  // Error handling for invalid combination type
   }
 
-  // Create the cards
-  const cards = ranks.split(' ').map((rank, index) => {
-    return new Card(suits[index], rank); // Create cards with the required suits
-  });
+  // Create the cards based on the ranks and suits
+  const cards = ranks.split( ' ' ).map( ( rank, index ) => new Card( suits[ index ], rank ) );
 
-  // console.log('Cards created:', cards);
   return { cards };
 }
